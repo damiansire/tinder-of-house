69d174191b2ae0cbb7af948607dec2ede8afb094
damiansire <damiansire@hotmail.com>
2021-02-18 03:30:04 -0300

    last version

--- api/mercadolibreScrapper.py
+++ api/mercadolibreScrapper.py
@@ -1,6 +1,4 @@
-
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
 import bdDao;
 import math;
 import time;
@@ -20,44 +18,20 @@ def get_address(data):
 
 ##Buscar alternativas a los try except, algo que contrele la excepion como el "?"" javascript
 ## Es esto: https://www.php2python.com/wiki/function.isset/
-def get_house_data(selenium_element):
-    try:
-        img = selenium_element.find_element_by_tag_name("img").get_attribute('src')
-    except Exception:
-        img = ""
-    try:
-        price = selenium_element.find_element_by_class_name("price-tag-fraction").text
-    except Exception:
-        price = ""  
-    try:
-        size = selenium_element.find_element_by_class_name("ui-search-card-attributes").text.replace("\n", " ")
-    except Exception:
-        size = ""   
-    direction = get_address(selenium_element.find_element_by_class_name("ui-search-item__location").text)
+def get_house_data(html_element):
+    img = html_element.find("img")['data-src']
+    if(img == ""):
+        img = html_element.find("img")['src']
+    price = html_element.find(class_="price-tag-fraction").text
+    size = html_element.find(class_="ui-search-card-attributes").text.replace("\n", " ")
+    direction = get_address(html_element.find(class_="ui-search-item__location").text)
     address = direction["address"] 
     city = direction["city"] 
     region = direction["region"]
-    try:
-        title = selenium_element.find_element_by_class_name("ui-search-item__title").text
-    except Exception:
-        title = ""  
-    try:
-        url = selenium_element.find_element_by_tag_name("a").get_attribute("href")
-    except Exception:
-        url = ""    
-    try:
-        code = url.split("-")[1]
-    except Exception:
-        code = ""   
+    title = html_element.find(class_="ui-search-item__title").text
+    url = html_element.a['href']
+    code = url.split("-")[1]
     return {'code' : code, 'imgurl' : img, 'price' : price, 'size' : size, 'address' : address, 'city' : city, 'region' : region, 'title' : title, 'url' : url}
-
-def selenium_houses_to_obj(all_house):
-    my_house_obj = []
-    for house in all_house:
-        houseObj = get_house_data(house)
-        print(houseObj)
-        my_house_obj.append(houseObj)
-    return my_house_obj
 
 def obtain_page_number(driver):
     #[u'1', u'049']
@@ -67,13 +41,18 @@ def obtain_page_number(driver):
     page_number = math.ceil(number / 48)
     return page_number
 
-def get_page(pageNumber):
-    return "https://listado.mercadolibre.com.uy/inmuebles/casas/_Desde_" + str(48*pageNumber+1) + "_PriceRange_8000UYU-14000UYU"
+def get_page(page_number):
+    return "https://listado.mercadolibre.com.uy/inmuebles/casas/_Desde_" + str(48*page_number+1) + "_PriceRange_8000UYU-14000UYU"
+
+def div_houses_to_obj(all_house):
+    my_house_obj = []
+    for house in all_house:
+        houseObj = get_house_data(house)
+        print(houseObj)
+        my_house_obj.append(houseObj)
+    return my_house_obj
 
 options = Options()
-#options.headless = True
-#options.add_argument('--disable-gpu')
-#options.add_argument('--no-sandbox')
 
 driver = webdriver.Firefox(executable_path = 'geckodriver', options=options)
 
@@ -81,14 +60,19 @@ endpoint = get_page(0)
 driver.get(endpoint)
 
 #Obtiene el numero de paginas
-pageNumber = obtain_page_number(driver)
+page_number = obtain_page_number(driver)
 
-for actualPageNumber in range(0,pageNumber):
-    driver.get(get_page(actualPageNumber))
-    time.sleep(30)
+from bs4 import BeautifulSoup
+
+for actual_page_number in range(0,page_number):
+    driver.get(get_page(actual_page_number))
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'lxml')
     #Agarro todas las publicaciones disponibles
-    all_house = driver.find_elements_by_class_name("ui-search-layout__item")
+    all_houses = soup.find_all("li", class_="ui-search-layout__item")
     #Las convierte a obj interno
-    all_houses_obj = selenium_houses_to_obj(all_house)
+    all_houses_obj = div_houses_to_obj(all_houses)
+
+for house_data in all_houses_obj:
     #Inserta en la base de datos
     bdDao.insert_all_house(all_houses_obj)
